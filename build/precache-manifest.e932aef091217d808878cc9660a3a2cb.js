690b01c697663a19eb5a01057a2ac2a03daec5d6
Valeriy <waliush@mail.ru>
2020-04-02 13:38:13 +0300

    added master

---
+++ build/precache-manifest.e932aef091217d808878cc9660a3a2cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3266e20550561261445f",
+    "url": "/static/js/main.28f0455a.chunk.js"
+  },
+  {
+    "revision": "02b0a59e4d384cdbc6a7",
+    "url": "/static/js/2.1c8d0edb.chunk.js"
+  },
+  {
+    "revision": "3266e20550561261445f",
+    "url": "/static/css/main.8407a952.chunk.css"
+  },
+  {
+    "revision": "02b0a59e4d384cdbc6a7",
+    "url": "/static/css/2.3ac6d48a.chunk.css"
+  },
+  {
+    "revision": "3ee796ea563f088cb5be224155173ff5",
+    "url": "/index.html"
+  }
+];
